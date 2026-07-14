76a6744ef5f399325b9119fc9115c62774e494b0
setyoahroza-hub <setyoahroza@gmail.com>
2026-07-14 14:19:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-document.getElementById("nama").textContent = "TES BERHASIL";
+const id = new URLSearchParams(window.location.search).get("id");
+
+async function loadGuru() {
+  try {
+    const response = await fetch("./data.json");
+
+    console.log("Status:", response.status);
+
+    const data = await response.json();
+
+    console.log(data);
+
+    const guru = data.find(item => item.id === id);
+
+    console.log(guru);
+
+    if (!guru) {
+      document.getElementById("nama").textContent = "Guru tidak ditemukan";
+      return;
+    }
+
+    document.getElementById("foto").src = guru.foto;
+    document.getElementById("nama").textContent = guru.nama;
+    document.getElementById("jk").textContent = guru.jk;
+    document.getElementById("nik").textContent = guru.nik;
+    document.getElementById("tempat").textContent = guru.tempat;
+    document.getElementById("lahir").textContent = guru.lahir;
+    document.getElementById("nuptk").textContent = guru.nuptk;
+    document.getElementById("unit").textContent = guru.unit;
+
+  } catch (err) {
+    console.error(err);
+    document.getElementById("nama").textContent = err.message;
+  }
+}
+
+loadGuru();
