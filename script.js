f3ddf472be08b0a18ebe33f470bd5183edaafb48
setyoahroza-hub <setyoahroza@gmail.com>
2026-07-14 13:57:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Website berjalan");
+const params = new URLSearchParams(window.location.search);
+
+const id = params.get("id");
+
+fetch("data.json")
+
+.then(res=>res.json())
+
+.then(data=>{
+
+    const guru = data.find(g=>g.id===id);
+
+    if(!guru){
+
+        document.body.innerHTML="<h2>Data guru tidak ditemukan</h2>";
+
+        return;
+
+    }
+
+    document.getElementById("foto").src=guru.foto;
+
+    document.getElementById("nama").textContent=guru.nama;
+
+    document.getElementById("jk").textContent=guru.jk;
+
+    document.getElementById("nik").textContent=guru.nik;
+
+    document.getElementById("tempat").textContent=guru.tempat;
+
+    document.getElementById("lahir").textContent=guru.lahir;
+
+    document.getElementById("nuptk").textContent=guru.nuptk;
+
+    document.getElementById("unit").textContent=guru.unit;
+
+});
